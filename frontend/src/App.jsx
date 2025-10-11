f1e4a2be14640f73213d6d659433732fb4ba7f8c
SanjeevKanth03 <220701250@rajalakshmi.edu.in>
2025-10-11 09:28:33 +0530

    fronted

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,11 +7,12 @@ import CounselorInterface from './pages/CounselorInterface.jsx';
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<LoginPage />} />
-      <Route path="/student-dashboard" element={<StudentDashboard />} />
-      <Route path="/counselor-dashboard" element={<CounselorInterface />} />
-    </Routes>
+    // <Routes>
+    //   <Route path="/" element={<LoginPage />} />
+    //   <Route path="/student-dashboard" element={<StudentDashboard />} />
+    //   <Route path="/counselor-dashboard" element={<CounselorInterface />} />
+    // </Routes>
+    <StudentDashboard/>
   );
 }
 
